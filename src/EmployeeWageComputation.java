1cf8f603583e88f9d3bcb7737d5a5b58a2ff1511
angiratiwari <angiratiwari1102@gmail.com>
2024-12-17 19:40:53 +0530

    UC-1 added

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,9 +1,21 @@
 import java.util.Random;
 
 public class EmployeeWageComputation {
-
     public static void main(String[] args) {
+
         System.out.println("Welcome to Employee Wage Computation Program!");
+
+        // UC 1: Check Employee is Present or Absent
+        checkEmployeeAttendance();
+    }
+    // UC 1: Check Employee Attendance
+    private static void checkEmployeeAttendance() {
+        int attendance = new Random().nextInt(2); // Random 0 or 1
+        if (attendance == 1) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
 
